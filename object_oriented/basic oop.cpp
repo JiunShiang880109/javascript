d75255bc347c7f340346f0d32e13f1a1d2f2be3d
JiunShiang880109 <71368367+JiunShiang880109@users.noreply.github.com>
2025-02-07 14:39:03 +0800

    2025/02/07 first commit

--- object_oriented/basic oop.cpp
+++ object_oriented/basic oop.cpp
@@ -3,16 +3,34 @@
 
 using namespace std;
 
-class Cat {
+class Cat{
    public://公開成員，可從外部調用
-    Cat(string name) {
+    /*Cat(string name) {
         this->name = name;//this代表指標，調用物件成員的方式是用->
         this->barking = "meow";
     }
     void saySomething() {
         cout << this->name << ": " << this->barking
              << endl;
+    }*/
+    
+   //慣用寫法
+    string getName(){
+        return this->name;
     }
+    /*string getbark(){
+        return this->barking;
+    } */
+
+    void setName(string name){
+        if(name.length()>10){
+            cerr<<"name.length() should <=10"<<endl;
+        }
+        this->name=name;
+    }
+/*     void setbark(string barking){
+        this->barking=barking;
+    } */
 
    private://私人成員，無法從外部調用
     string name;
@@ -20,11 +38,13 @@ class Cat {
 };
 
 int main() {
-    Cat c1("Alice");
-    Cat c2("Bob");
+    Cat c1;
+    Cat c2;
 
-    c1.saySomething();
-    c2.saySomething();
-
+    c1.setName("Alice");
+    c2.setName("Bob");
+    
+    cout<<c1.getName()<<c2.getName()<<endl;//Alice
+    
     return 0;
 }
